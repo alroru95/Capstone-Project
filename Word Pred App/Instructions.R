b4de32ec234bb71e78f4ba059e11c376abd51891
A. Rodríguez <66773906+alroru95@users.noreply.github.com>
2021-02-06 21:00:40 +0100

    Create Instructions.R

---
+++ Word Pred App/Instructions.R
@@ -0,0 +1,19 @@
+<html>
+  <head></head>
+  <body>
+  
+  <h4>Overview</h4>    
+  <div>
+    <p>Next Word Predictor Application suggests the most probable word after entering a word. Only words in English are allowed.</p>
+  </div>
+
+  <h4>Instructions</h4>
+  <div>
+    <p><li>Enter a word or phrase, in English, into the textbox titled "Input".</li></p>
+    <p><li>Wait a split second.</li></p>
+    <p><li>A "suggested next word" will appear below the textbox.</li></p>
+    <p><li>The word/phrase entered will appear below the suggested next word.</li></p>
+  </div>
+
+</body>
+</html>
